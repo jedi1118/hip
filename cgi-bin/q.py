4ee762c10bbfbbf6188d010e0bf23412318ddb5b
Shane Lin <pobox.uno@gmail.com>
2023-10-16 13:23:22 -0700

    rm hardcoded html, replace with hardcoded test data

--- cgi-bin/q.py
+++ cgi-bin/q.py
@@ -1,11 +1,11 @@
 import csv
 # transform a single question and answer,
-#
+# TODO: instead of return array data - q and a all together, concert to JSON
 def transformData(data):
     print ("Content-type:application/json\r\n\r\n")
     print (data)
 
-
+# TODO: this need to get data from ai - all py api???
 if __name__=="__main__":
     # Parse the CSV file
     with open("./cgi-bin/testbench.csv", "r") as csvfile:
@@ -13,7 +13,3 @@ if __name__=="__main__":
         headers = next(reader)
         data = list(reader)
         transformData(data[0]);
-        
-
-
-
--- src/Question.js
+++ src/Question.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './Question.css';
 
 class Question extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};// game: this.props.cells.slice(), selected: []};
-        // this.handleSquareReset = this.handleSquareReset.bind(this);
+        // hardcode for now, should come from ai/be
+        const q = [2,'Insulin produced by molecular cloning:','is of pig origin','is a recombinant protein','is made by the human pancreas','is recombinant DNA,is a recombinant protein'];        
+        this.state = {
+            quesion: q[1],
+            answers: q.slice(2)
+        };
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(idx) {
 
     }
+
     render () {
         return <>
-                <div>GMOs are created by ________?</div>
+                <div className="Title">{this.state.quesion}</div>
                 <ul>
-                    <li><input type="checkbox"></input>generating genomic DNA fragments with restriction endonucleases</li>
-                    <li><input type="checkbox"></input>introducing recombinant DNA into an organism by any means</li>
-                    <li><input type="checkbox"></input>overexpressing proteins in E. coli</li>
-                    <li><input type="checkbox"></input>all of the above</li>
-                    <li><input type="checkbox"></input>introducing recombinant DNA into an organism by any means</li>
+                {this.state.answers.map((itm) => { 
+                    return <li><input type="checkbox"></input><span>{itm}</span></li> })}
                 </ul>
                 <inpu className="Submit" type="button">Submit</inpu>
             </>
